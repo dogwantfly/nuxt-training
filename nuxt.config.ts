13b6099ffdc4fc73ca41143c53af4b987abca962
hsin yu <tina2793778@gmail.com>
2024-05-20 17:28:56 +0800

    feat: 🎸 ch3 SEO Meta 設定

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -20,4 +20,22 @@ export default defineNuxtConfig({
       },
     ],
   ],
+  app: {
+    head: {
+      viewport: 'width=500, initial-scale=1',
+      title: 'Nuxt3 training',
+      meta: [
+        {
+          name: 'description',
+          content: 'Nuxt3 training Repo',
+        },
+        { property: 'og:title', content: 'Nuxt3 training' },
+        { property: 'og:url', content: 'http://localhost:3000/' },
+        {
+          property: 'og:description',
+          content: 'Nuxt3 training',
+        },
+      ],
+    },
+  },
 });
